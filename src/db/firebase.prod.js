f3aa8578c635bd341dd1028fa69d04c64a9da17d
BivalentContinew <kwapeter@gmail.com>
2021-04-26 10:48:55 -0400

    removed config

--- src/db/firebase.prod.js
+++ src/db/firebase.prod.js
@@ -12,15 +12,6 @@ import 'firebase/auth'
 import { config } from '../config.js'
 
 
-// const configKeys = {
-//   apiKey: "AIzaSyCam_3lLPEVUUJZrLmahL73f0T4Dha4t14",
-//   authDomain: "video-subscription-player.firebaseapp.com",
-//   projectId: "video-subscription-player",
-//   storageBucket: "video-subscription-player.appspot.com",
-//   messagingSenderId: "273317949225",
-//   appId: "1:273317949225:web:fc7722dc648964a9e85138"
-// }
-
 
 const configKeys = {
   apiKey: config.apiKey,
